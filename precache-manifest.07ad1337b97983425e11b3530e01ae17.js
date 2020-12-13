d3fd961bd40c589de98bc8bb219f3badba09cdb3
asoldatov <andrey.soldatov@infobip.com>
2020-12-13 17:22:54 +0300

    build

---
+++ precache-manifest.07ad1337b97983425e11b3530e01ae17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "475b0e8524f9aae84e2333bc991d7873",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d91cecf1e86343b8c11c",
+    "url": "/static/css/main.12e4e6ed.chunk.css"
+  },
+  {
+    "revision": "bc3ff80211c5c05c7510",
+    "url": "/static/js/2.b209ffe5.chunk.js"
+  },
+  {
+    "revision": "569e878394263cd68864b6526c81f10b",
+    "url": "/static/js/2.b209ffe5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d91cecf1e86343b8c11c",
+    "url": "/static/js/main.17887c6c.chunk.js"
+  },
+  {
+    "revision": "08f73d5cd9e484c26cef",
+    "url": "/static/js/runtime-main.7bb332b4.js"
+  }
+]);
